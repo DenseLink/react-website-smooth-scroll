8a3d9681b695440444b6006925376a258d1726b0
DenseLink <tardis1103@gmail.com>
2023-03-18 12:05:28 -0400

    added services and moved images

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
 import Footer from '../components/Footer';
+import Services from '../components/Services';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,6 +21,7 @@ const Home = () => {
         <HeroSection />
         <InfoSection {...homeObjOne}/>
         <InfoSection {...homeObjTwo}/>
+        <Services />
         <InfoSection {...homeObjThree}/>
         <Footer />
     </>
