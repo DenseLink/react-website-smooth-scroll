65fb0b9442c5405c0667164137e8129bdd642fe9
DenseLink <tardis1103@gmail.com>
2023-03-18 12:28:08 -0400

    sign in page setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages';
+import SignIn from './pages/signin';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} exact />
+        <Route path='/signin' element={<SignIn />} exact />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+//tutorial here: https://www.youtube.com/watch?v=Nl54MJDR2p8
